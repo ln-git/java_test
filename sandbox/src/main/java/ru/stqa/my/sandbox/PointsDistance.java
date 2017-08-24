c3a91bfad339a362c1d9a569e76026d5f8e906e7
ln-git <rita@github.com>
2017-08-25 02:02:04 +0300

    Вычисление расстояния между двумя точками

--- sandbox/src/main/java/ru/stqa/my/sandbox/PointsDistance.java
+++ sandbox/src/main/java/ru/stqa/my/sandbox/PointsDistance.java
@@ -6,11 +6,8 @@ public class PointsDistance {
     Point p1 = new Point(4,5);
     Point p2 = new Point (7,8);
 
-    System.out.println("Расстояние между двумя точками =" + distance(p1, p2));
+    System.out.println("Расстояние между двумя точками =" + p2.distance(p1));
   }
 
-  public static double distance(Point p1, Point p2){
 
-    return Math.sqrt(Math.pow(p2.x-p1.x,2)+Math.pow(p2.y-p1.y,2));
-  }
 }
