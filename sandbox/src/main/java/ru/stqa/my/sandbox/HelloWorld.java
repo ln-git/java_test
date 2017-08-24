49e1cb153d19204ef05193856d645ffec5999235
ln-git <rita@github.com>
2017-08-24 23:40:47 +0300

    Добавлены функции вычисления площади квадрата и прямоугольника

--- sandbox/src/main/java/ru/stqa/my/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/my/sandbox/HelloWorld.java
@@ -3,7 +3,26 @@ package ru.stqa.my.sandbox;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("world");
+    hello("user");
+    hello("Margarita");
+
+    double l=5;
+    double a=4;
+    double b=6;
+    System.out.println("Площадь квадрата со стороной "+l+" = "+ area(l));
+    System.out.println("Площадь прямоугольника со сторонами "+a+" и "+b+" = "+ area(a,b));
   }
 
+  public static void hello(String somebody){
+    System.out.println("Hello, "+somebody+" !");
+  }
+
+  public static double area (double l){
+    return l*l;
+  }
+
+  public static double area (double a, double b){
+    return a*b;
+  }
 }
